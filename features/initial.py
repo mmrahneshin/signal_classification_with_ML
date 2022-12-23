e30f2023e57ddfe270283de5d30b86ee36916886
mmrahneshin <mmrahneshin@gmail.com>
2022-12-23 16:18:26 +0330

    time domain features

--- features/initial.py
+++ features/initial.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from features.statistical import *
+from features.timeDomain import *
 
 def initialize_features(dataset):
     features_dataset = []
@@ -15,6 +16,15 @@ def initialize_features(dataset):
         features.append(deviation(data))
         features.append(rms(data))
         # statistical --------------------------
+
+        # timeDomain --------------------------
+        features.append(min_max_distance(data))
+        features.append(skewness(data))
+        features.append(kurtosis_calc(data))
+        features.append(margin_factor(data))
+        features.append(crest_factor(data))
+        # timeDomain --------------------------
+
 
         features_dataset.append(features)
 
